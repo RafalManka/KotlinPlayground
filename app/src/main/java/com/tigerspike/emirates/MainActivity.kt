fc42fd30595436aa1645beea2abaa9bb1f56cfcf
rafal.manka <rafal@manka.info>
2018-05-19 23:51:24 +0400

    Simple binding extension

--- app/src/main/java/com/tigerspike/emirates/MainActivity.kt
+++ app/src/main/java/com/tigerspike/emirates/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.tigerspike.emirates
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.IdRes
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    val recyclerView: RecyclerView by bind(R.id.recyclerView)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
+
+private fun <V : View> AppCompatActivity.bind(@IdRes id: Int): Lazy<V> {
+    return lazy { findViewById<V>(id) }
+}
+
+
