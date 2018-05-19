e35d1ee327b11f1d0ba39c479332169203f847aa
rafal.manka <rafal@manka.info>
2018-05-19 23:04:35 +0400

    app dependencies completed

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,6 +3,9 @@ object Versions {
     const val kotlin = "1.2.41"
     const val appCompatV7 = "27.1.1"
     const val jUnit = "4.12"
+    const val recyclerView = "27.1.1"
+    const val lifecycle = "1.1.1"
+    const val retrofit = "2.4.0"
 }
 
 object Plugins {
@@ -11,4 +14,7 @@ object Plugins {
     const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
     const val appCompatV7 = "com.android.support:appcompat-v7:${Versions.appCompatV7}"
     const val jUnit = "junit:junit:${Versions.jUnit}"
+    const val recyclerView = "com.android.support:recyclerview-v7:${Versions.recyclerView}"
+    const val lifecycle = "android.arch.lifecycle:extensions:${Versions.lifecycle}"
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
 }
