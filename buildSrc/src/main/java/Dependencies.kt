e5acad22b93b56309334fe759abaeedc0856768f
rafal.manka <rafal@manka.info>
2018-05-19 22:50:01 +0400

    App gradle setup

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,10 +1,14 @@
 object Versions {
-    val gradle = "3.1.2"
-    val kotlin = "1.2.41"
+    const val gradle = "3.1.2"
+    const val kotlin = "1.2.41"
+    const val appCompatV7 = "27.1.1"
+    const val jUnit = "4.12"
 }
 
 object Plugins {
-    val gradleBuild = "com.android.tools.build:gradle:${Versions.gradle}"
-    val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
-    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    const val gradleBuild = "com.android.tools.build:gradle:${Versions.gradle}"
+    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    const val appCompatV7 = "com.android.support:appcompat-v7:${Versions.appCompatV7}"
+    const val jUnit = "junit:junit:${Versions.jUnit}"
 }
