002c426cdb3d626562e0d60b5391200f7abea5c2
rafal.manka <rafal@manka.info>
2018-06-18 13:44:28 +0400

    Named functions instead of variable calls

--- app/src/main/java/com/tigerspike/emirates/feature/airports/AirportsViewModel.kt
+++ app/src/main/java/com/tigerspike/emirates/feature/airports/AirportsViewModel.kt
@@ -32,22 +32,6 @@ class AirportsViewModel(
     private var filterBy: String = ""
     private var isRunning: Boolean = false
 
-    private val airportComparator: Comparator<in Airport> =
-            Comparator { left, right ->
-                when {
-                    left.name ?: "" > right.name ?: "" -> 1
-                    left.name == right.name -> 0
-                    else -> -1
-                }
-            }
-
-    private val airportsFilter: (Airport) -> Boolean = {
-        it.name?.isNotBlank() == true
-                && !it.name.contains("?")
-                && it.type == "airport"
-                && (it.size == "large" || it.size == "medium")
-    }
-
     fun getAirports(filter: String = "") {
         filterBy = filter
         airportDb.count()
@@ -55,20 +39,15 @@ class AirportsViewModel(
                     if (count == 0) {
                         refreshAirports({ getAirports(filterBy) }, { it?.logWarning() })
                     } else {
-                        airportDb.(getConditionalQuery(filter))()
+                        val query = getConditionalQuery(filter)
+                        airportDb
+                                .query()
                                 .observe(lifecycleOwner, Observer {
                                     airports.postValue(it)
                                 })
                     }
                 })
     }
-
-    private fun getConditionalQuery(filter: String): AirportDao.() -> LiveData<Array<Airport>> =
-            if (filter.isEmpty()) {
-                { fetchAll() }
-            } else {
-                { fetchAllMatching("%$filter%") }
-            }
 
     private fun refreshAirports(onSuccess: (Array<Airport>) -> Unit, onError: (Throwable?) -> Unit) {
         if (isRunning) {
@@ -84,9 +63,9 @@ class AirportsViewModel(
                             error.postValue("")
                             val airports = response
                                     ?.body()
-                                    ?.filter(airportsFilter)
-                                    ?.toTypedArray() ?: emptyArray()
-                            airports.sortWith(airportComparator)
+                                    ?.removeInvalidAirports()
+                                    ?: emptyArray()
+                            airports.sortAirports()
                             airportDb.insert(airports)
                             isRunning = false
                             onSuccess(airports)
@@ -103,6 +82,31 @@ class AirportsViewModel(
     }
 
 }
+
+private fun Array<Airport>.sortAirports() {
+    sortWith(Comparator { left, right ->
+        when {
+            left.name ?: "" > right.name ?: "" -> 1
+            left.name == right.name -> 0
+            else -> -1
+        }
+    })
+}
+
+private fun getConditionalQuery(filter: String): AirportDao.() -> LiveData<Array<Airport>> =
+        if (filter.isEmpty()) {
+            { fetchAll() }
+        } else {
+            { fetchAllMatching("%$filter%") }
+        }
+
+private fun Array<Airport>.removeInvalidAirports(): Array<Airport> =
+        filter {
+            it.name?.isNotBlank() == true
+                    && !it.name.contains("?")
+                    && it.type == "airport"
+                    && (it.size == "large" || it.size == "medium")
+        }.toTypedArray()
 
 
 
